c7199bd6ee5f39fe9d2bc8ed22805a0ee5a5db12
RoninLexx <lexxpsycho@yandex.ru>
2024-11-08 12:32:15 +0700

    Add files via upload

--- module_10_1.py
+++ module_10_1.py
@@ -1,5 +1,7 @@
 import time
 import threading
+from datetime import datetime
+
 
 def write_words(word_count, file_name):
     with open(file_name, 'w', encoding='utf-8') as file:
@@ -12,15 +14,15 @@ def write_words(word_count, file_name):
 
 
 if __name__ == '__main__':
-    start_time = time.time()
+    start_time = datetime.now()
     write_words(10, 'example1.txt')
     write_words(30, 'example2.txt')
     write_words(200, 'example3.txt')
     write_words(100, 'example4.txt')
-    end_time = time.time()
+    end_time = datetime.now()
     w_t = end_time - start_time
-    print(f'Работа потоков {round(w_t, 4)}')
-    start_time = time.time()
+    print(f'Работа потоков {w_t}')
+    start_time = datetime.now()
     threads = []
     for count, name in [(10, 'example5.txt'), (30, 'example6.txt'), (200, 'example7.txt'), (100, 'example8.txt')]:
         thread = threading.Thread(target=write_words, args=(count, name))
@@ -28,8 +30,6 @@ if __name__ == '__main__':
         thread.start()
     for thread in threads:
         thread.join()
-    end_time = time.time()
+    end_time = datetime.now()
     w_t = end_time - start_time
-    print(f'Работа потоков {round(w_t, 4)}')
-
-
+    print(f'Работа потоков {w_t}')
